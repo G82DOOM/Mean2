b1b42a49f0538fec2667044cf092d5ccf3aa52aa
G82DOOM <71901005+G82DOOM@users.noreply.github.com>
2022-01-07 01:59:58 +0200

    Update todos.service.ts

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -7,7 +7,7 @@ import { Todo } from '../models/todo';
 })
 export class TodosService {
 
-  private baseUrl : string ="http://mean-2-andrew3.apps.eu45.prod.nextcle.com/";
+  private baseUrl : string ="http://backend-andrew3.apps.eu45.prod.nextcle.com/";
 
   constructor(private _HttpClient: HttpClient) { }
 
